b76d83ff9d52382886407b44dc6fecf6e3040299
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-02-03 15:45:54 +0100

    modif for mesonet

--- retinotopy.py
+++ retinotopy.py
@@ -168,7 +168,7 @@ elif 'DESKTOP-27VNO0E' in HOST:
     DATAROOT = 'd:\\Data'
     batch_size = 50
     num_workers = 8
-elif 'm-gpu01' in HOST: 
+elif 'm-gpu' in HOST: 
     DATAROOT = 'data'
     batch_size = 50
     num_workers = 1
