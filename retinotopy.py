fc9571cbd33235f61df611927ad6f143bb8b9be0
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-03-23 15:42:30 +0100

    Suppression de l'argument map_location lors du chargement de l'état du modèle dans la fonction apply_weights.

--- retinotopy.py
+++ retinotopy.py
@@ -786,7 +786,7 @@ def apply_weights(model, model_path, verbose=True):
     if verbose: print(f'loading .... {model_path}')
     # model.load_state_dict(torch.load(model_path), map_location=torch.device(device), weights_only=True)
     # model.load_state_dict(torch.load(model_path), weights_only=True)
-    model.load_state_dict(torch.load(model_path), map_location=torch.device(device))
+    model.load_state_dict(torch.load(model_path))
     return model
 
 import torchvision.models as models
