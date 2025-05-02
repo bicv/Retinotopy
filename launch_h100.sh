3da97aad69e92a10b90e0985bb06b4fdc66b24c1
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-05-02 12:45:42 +0200

    Ajout de scripts de lancement pour les environnements GPU h100 et v100

--- launch_h100.sh
+++ launch_h100.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #SBATCH -J Retino
-#SBATCH  -A fsx@v100
-#SBATCH  -C v100-32g
-#SBATCH --qos=qos_gpu-t4
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=10
+#SBATCH  -A fsx@h100
+#SBATCH  -C h100
+#SBATCH --qos=qos_gpu_h100-t3
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
 #SBATCH --gres=gpu:1
 #SBATCH --hint=nomultithread
 #SBATCH -t 40:00:00 
 #SBATCH -o cached_data/log_%j_out.log  # <-- the name of the file where the output of the simulation is written
 #SBATCH -e cached_data/log_%j_err.log  # <-- the name of the file where errors of the simulation are written
-# https://mesocentre.univ-amu.fr/slurm/
 
 module purge
+make module load arch/h100
 module load pytorch-gpu/py3
 
 make
---
+++ launch_v100.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#SBATCH -J Retino
+#SBATCH  -A fsx@v100
+#SBATCH  -C v100-32g
+#SBATCH --qos=qos_gpu-t4
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:1
+#SBATCH --hint=nomultithread
+#SBATCH -t 40:00:00 
+#SBATCH -o cached_data/log_%j_out.log  # <-- the name of the file where the output of the simulation is written
+#SBATCH -e cached_data/log_%j_err.log  # <-- the name of the file where errors of the simulation are written
+
+
+module purge
+module load pytorch-gpu/py3
+
+make
