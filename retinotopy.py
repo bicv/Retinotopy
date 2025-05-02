79fcf8d0eb3f857bee4325a8c63dc69c34793b79
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-05-02 13:00:31 +0200

    Augmenter la taille du batch à 256 pour une meilleure performance sur Jean Zay

--- retinotopy.py
+++ retinotopy.py
@@ -145,7 +145,7 @@ if USER=='uvb28bo': # Jean Zay
     # DATAROOT = '../data'
     DATAROOT = f'{os.environ['SCRATCH']}/data'
     num_workers = 8
-    batch_size = 128
+    batch_size = 256
     print(f'Running on Jean Zay with {torch.cuda.get_device_name()} with {DATAROOT=} and {USER=} ')
 elif '.cluster' in HOST: # mesocentre
     DATAROOT = '/scratch/lperrinet/science/Deep_learning/data'
