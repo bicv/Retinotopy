56890ae402f32d138210d00db504a775073cbe94
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-07-31 11:20:37 +0200

    Mise à jour de la variable DATAROOT pour l'utilisateur Jean Zay et nettoyage des conditions d'environnement

--- retinotopy.py
+++ retinotopy.py
@@ -145,54 +145,54 @@ USER = os.environ['USER']  # username
 
 # platform-dependent variables
 if USER=='uvb28bo': # Jean Zay
-    # DATAROOT = f'{os.environ['WORK']}/DeepLearningDatasets' # TODO
-    DATAROOT = f'{os.environ['SCRATCH']}/data'
+    DATAROOT = f'{os.environ['WORK']}/DeepLearningDatasets' # TODO
+    # DATAROOT = f'{os.environ['SCRATCH']}/data'
     # num_workers = 16 # on H100
     num_workers = 8 # on V100
     print(f'Running on Jean Zay with {torch.cuda.get_device_name()} with {DATAROOT=} and {USER=} ')
-elif '.cluster' in HOST: # mesocentre
-    DATAROOT = '/scratch/lperrinet/science/Deep_learning/data'
-    num_workers = 8
-elif 'm-gpu' in HOST: 
-    DATAROOT = 'data'
-    # batch_size = 50
-    num_workers = 2
-elif HOST in ['babbage']: # 
-    DATAROOT = '/data/Deep_learning/data'
-    num_workers = 2
-elif HOST in ['CONECT-LID-01']: # emmy
-    # DATAROOT = '/envau/userspace/perrinet.l/data'
-    DATAROOT = '/scratch/data'
-    batch_size = 256
-    num_workers = 8    
-elif HOST in ['CONEC-LID-002']: # faraday
-    # DATAROOT = '/envau/userspace/perrinet.l/data'
-    DATAROOT = '/scratch/ImageNet'
-    num_workers = 16    
-elif HOST in ['inv-ope-de06', 'INV-133-DE01']: # CURIE , ada
-    DATAROOT = '/data/JNJER/Deep_learning/data'
-    num_workers = 2
+# elif '.cluster' in HOST: # mesocentre
+#     DATAROOT = '/scratch/lperrinet/science/Deep_learning/data'
+#     num_workers = 8
+# elif 'm-gpu' in HOST: 
+#     DATAROOT = 'data'
+#     # batch_size = 50
+#     num_workers = 2
+# elif HOST in ['babbage']: # 
+#     DATAROOT = '/data/Deep_learning/data'
+#     num_workers = 2
+# elif HOST in ['CONECT-LID-01']: # emmy
+#     # DATAROOT = '/envau/userspace/perrinet.l/data'
+#     DATAROOT = '/scratch/data'
+#     batch_size = 256
+#     num_workers = 8    
+# elif HOST in ['CONEC-LID-002']: # faraday
+#     # DATAROOT = '/envau/userspace/perrinet.l/data'
+#     DATAROOT = '/scratch/ImageNet'
+#     num_workers = 16    
+# elif HOST in ['inv-ope-de06', 'INV-133-DE01']: # CURIE , ada
+#     DATAROOT = '/data/JNJER/Deep_learning/data'
+#     num_workers = 2
 elif HOST in ['neo-ope-de04']: # Darwin  
     DATAROOT = '/data/JNJER/Deep_learning/data'
     num_workers = 16
-elif HOST in ['brain-lid-004']: # GPU manu  
-    DATAROOT = '/data/JNJER/Deep_learning/data'
-    num_workers = 16
-elif 'obiwan' in HOST: 
-    # DATAROOT = '/Volumes/UnaTera/2023_archives/2023_science/JNJER_PhD/data'
-    DATAROOT = '/Volumes/data/2024_archives/2024_science/Deep_learning/data'
-    DATAROOT = '/Volumes/SSD1TO/ImageNet'
-    DATAROOT = '/Volumes/SSD1TO/Deep_learning/ILSVRC2010_ImageNet'
-    DATAROOT = 'data'
-    DATAROOT = '/Volumes/SSD1TO/DeepLearningDatasets'
-    interpolation = T.InterpolationMode.NEAREST
-    padding_mode = "reflection"
-    num_workers = 2
-elif 'Ahsoka' in HOST: 
-    DATAROOT = '/Volumes/backups/2023_archives/2023_science/JNJER_PhD/data'
-    DATAROOT = '/Volumes/data/2024_archives/2024_science/Deep_learning/data'
-    num_workers = 24
-    device = torch.device('cpu')
+# elif HOST in ['brain-lid-004']: # GPU manu  
+#     DATAROOT = '/data/JNJER/Deep_learning/data'
+#     num_workers = 16
+# elif 'obiwan' in HOST: 
+#     # DATAROOT = '/Volumes/UnaTera/2023_archives/2023_science/JNJER_PhD/data'
+#     DATAROOT = '/Volumes/data/2024_archives/2024_science/Deep_learning/data'
+#     DATAROOT = '/Volumes/SSD1TO/ImageNet'
+#     DATAROOT = '/Volumes/SSD1TO/Deep_learning/ILSVRC2010_ImageNet'
+#     DATAROOT = 'data'
+#     DATAROOT = '/Volumes/SSD1TO/DeepLearningDatasets'
+#     interpolation = T.InterpolationMode.NEAREST
+#     padding_mode = "reflection"
+#     num_workers = 2
+# elif 'Ahsoka' in HOST: 
+#     DATAROOT = '/Volumes/backups/2023_archives/2023_science/JNJER_PhD/data'
+#     DATAROOT = '/Volumes/data/2024_archives/2024_science/Deep_learning/data'
+#     num_workers = 24
+#     device = torch.device('cpu')
 elif 'DESKTOP-27VNO0E' in HOST: 
     DATAROOT = 'd:\\Data'
     batch_size = 50
