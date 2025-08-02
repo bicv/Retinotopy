a5c89d21403ad2010098b072bf462fe6401f3d4f
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-08-02 11:40:34 +0200

    Corrected typo in `momentum` and `mutnemom` in `retinotopy.py`.
    This commit adjusts optimizer parameters in `retinotopy.py` to fix a typo related to momentum and beta2, ensuring proper training configuration.

--- retinotopy.py
+++ retinotopy.py
@@ -236,8 +236,8 @@ class Params:
     batch_size: int = batch_size # Set number of images per input batch
     batch_size_val: int = batch_size # Set number of images per input batch
     lr: float = 5.e-5 # Set learning rate 
-    momentum: float = .02 # Set the momentum
-    beta2: float = 0 # Set the second momentum - use SGD if set to 0
+    mutnemom: float = .02 # Set the momentum as 1 - mutnemom
+    beta2: float = 0 # Sets the second momentum - use SGD if set to 0
     rs_min: float = 0.00 # Set minimum radius of the log-polar grid
     rs_max: float = -5.00 # Set maximum radius of the log-polar grid
     
@@ -775,9 +775,9 @@ def train_model(args:dict, model, dataloaders:torch.utils.data.DataLoader, df_tr
 
     # sets the optimizer
     if args.beta2 > 0.: 
-        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(1-args.momentum, 1-args.beta2)) 
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(1-args.mutnemom, 1-args.beta2)) 
     else:
-        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=1-args.momentum) # to set training variables
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=1-args.mutnemom) # to set training variables
     
     # https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html 
     criterion = nn.CrossEntropyLoss() # binary_cross_entropy_with_logits
