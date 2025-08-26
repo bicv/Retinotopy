2f421e704d8ad18ebf7ddacb8aaa458361a8c9ae
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2025-08-26 12:17:55 +0200

    removing bracket

--- retinotopy.py
+++ retinotopy.py
@@ -147,7 +147,8 @@ USER = os.environ['USER']  # username
 if USER=='uvb28bo': # Jean Zay
     # work_dir = os.environ['WORK']
     # DATAROOT = f'{work_dir}/DeepLearningDatasets' 
-    DATAROOT = f'{os.environ['SCRATCH']}/data'
+    # DATAROOT = f'{os.environ['SCRATCH']}/data'
+    DATAROOT = f'/lustre/fsn1/projects/rech/fsx/uvb28bo/data' 
     # num_workers = 16 # on H100
     num_workers = 8 # on V100
     print(f'Running on Jean Zay with {torch.cuda.get_device_name()} with {DATAROOT=} and {USER=} ')
